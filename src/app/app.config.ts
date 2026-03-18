88ebc30924d7a62674e217d7cef2cafbeb38f462
Walter Emilio Delgado Yucra <110571114+WedyDev@users.noreply.github.com>
2026-03-18 14:02:44 -0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
@@ -10,17 +9,3 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withFetch())
   ]
-=======
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(withFetch())
-  ]
->>>>>>> Stashed changes
-};
