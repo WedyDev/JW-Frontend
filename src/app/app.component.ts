c0c504dc57e7ce066b59e8f85c5ff0127b324c69
Walter Emilio Delgado Yucra <110571114+WedyDev@users.noreply.github.com>
2026-03-18 14:03:01 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
@@ -12,18 +11,4 @@ import { HomeComponent } from "./components/home/home.component";
 export class AppComponent {
   title = 'Landing_jw_consulting';
 }
-=======
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./components/home/home.component";
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'Landing_jw_consulting';
-}
->>>>>>> Stashed changes
